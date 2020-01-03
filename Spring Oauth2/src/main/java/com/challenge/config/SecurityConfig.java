597eaad17b84a8c49f605ae1a1e2d41c01cdf1b5
guilherassis <guilher.assis@gmail.com>
2020-01-03 13:45:19 -0300

    Desafio Spring Oauth2

---
+++ Spring Oauth2/src/main/java/com/challenge/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.challenge.config;
+
+import com.challenge.service.impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@EnableWebSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+
+		auth.inMemoryAuthentication()
+				.withUser("guilherme")
+				.password("123456")
+				.roles("ADMIN");
+
+	}
+
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception{
+		return super.authenticationManager();
+
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+
+		return NoOpPasswordEncoder.getInstance();
+
+	}
+
+
+
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/user");
+	}
+
+
+	
+}
